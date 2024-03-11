9724dca75e35ec66e871a8d2aeb1d7c3a49bce13
cristiansot <soluciongrafix@gmail.com>
2024-03-10 21:43:14 -0300

    I have changed the json structure to access the objects through map

--- src/components/QuizGame.jsx
+++ src/components/QuizGame.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { useState, useEffect } from "react";
 import quizzes from '../quizzes.json'
 
-const [score, setScore] = useState();
-const [currentQuestion, setCurrentQuestion] = useState();
-const [gameIsOver, setGameIsOver] = useState(false);
-
 const QuizGame = () => {
+
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [gameIsOver, setGameIsOver] = useState(false);
   
     return (
       <div className='quizGameWrapper'>
         <div><h1>Quiz Title</h1></div>
-        <div><h2>Question</h2></div>
+        <div><h2>{currentQuestion + 1}</h2></div>
         <div className="answers">
           <button className="answerBtn">Option 1</button>
           <button className="answerBtn">Option 2</button>
