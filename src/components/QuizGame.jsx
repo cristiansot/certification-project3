5a07965bc1d201d9c72ee92ae6dfe95117390da2
cristiansot <soluciongrafix@gmail.com>
2024-03-10 21:07:37 -0300

    Quiz Game component created

---
+++ src/components/QuizGame.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import quizzes from '../quizzes.json'
+
+const QuizGame = () => {
+  
+    return (
+      <div className='quizGame'>
+        
+      </div>
+    );
+  };
+
+export default QuizGame;
