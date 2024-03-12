246bf0884c4e29937d5d0aeb25556159c2ab034c
cristiansot <soluciongrafix@gmail.com>
2024-03-12 12:31:54 -0300

    Added a create quiz button in quizzes section

--- src/components/Quizzes.jsx
+++ src/components/Quizzes.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import Crud from './Crud'
 import quiz from '../../data/quiz.json'
-
-
-// Create a button component that has the title as a prop, this will be the question that will read from the database
-
-//I need a function to constantly read the database to know if there are changes
-
-//I need a function that, once the quiz is created, diplay the new button on the screen
-
+import '../assets/css/quizzes.css'
 
 const Quizzes = () => {
-    return (
-        <div className='wrapperBtns'>
-            {quiz.map(quiz => (
-               <Crud 
-                key={quiz.id}
-                name={quiz.name}
-               />
-            ))}
-        </div>
-    );
+  return (
+    <>
+    <div className='contentBtn'>
+      <button className='createBtn'>Create Quiz</button>
+    </div>
+    <div className='wrapperBtns'>
+        {quiz.map(quiz => (
+          <Crud 
+          key={quiz.id}
+          name={quiz.name}
+          />
+        ))}
+    </div>
+    </>
+  );
 };
 
 export default Quizzes;
