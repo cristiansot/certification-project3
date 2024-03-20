8aa3bc5a34979cd2ed12df280360d778407c4da1
cristiansot <soluciongrafix@gmail.com>
2024-03-20 13:12:41 -0300

    Create form is doing a object, needs check box for to mark the true answer

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import FormQuestions from './FormQuestions';
+
 
 const Create = () => {
-//I need to create ID
+/*I need to create ID*/
   
   return (
     <div >
         <div className='contentInputsLogin'>
         <h1>Create Quiz</h1>
-          <input className='name' type="text" placeholder='Name of the Quiz' onChange={handleChange} />
-          //Import form questions
+          <input className='name' type="text" placeholder='Name of the Quiz' />
+    
         </div> 
+        <FormQuestions />
     </div>
   );
 };
--- src/components/FormQuestions.jsx
+++ src/components/FormQuestions.jsx
@@ -1,37 +1,53 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import '../assets/css/login.css'
+import '../assets/css/login.css';
 
 const schema = Yup.object({
   question: Yup.string().required('Required'),
-  textAnswer: Yup.string().required('Required'),
-  //I need to put the options inside an array
+  options: Yup.array().of(Yup.string().required('Required')),
 });
 
 const FormQuestions = () => {
-
   const send = (values) => {
-
+    console.log(values);
   };
   
   const { handleSubmit, handleChange, errors, values } = useFormik({
     initialValues: {
       question: '',
-      options: []
+      options: ['', '', '', ''] 
     },
     validationSchema: schema,
     onSubmit: send,
   });
 
+/** I saw this documentation and videos for to understand how to develop the form
+ * https://formik.org/docs/guides/arrays
+ * www.youtube.com/watch?v=Or0rljWUyEM&ab_channel=EWebik
+ * https://www.youtube.com/watch?v=7Ophfq0lEAY&ab_channel=NikitaDev
+**/
+
   return (
     <div className='wrapperLogin'>
       <form onSubmit={handleSubmit}>
         <div className='contentInputsLogin'>
-        <h1>Create Quiz</h1>
-          <input className='inputUserName' type="text" placeholder='Text Answer' name='textAnswer' onChange={handleChange} />
-          <div>{errors.textAnswer && <span>{errors.textAnswer}</span>}</div>
+          <h1>Create Questions</h1>
 
+          <input className='inputUserName' type="text" placeholder='Write the Question' name='question' value={values.question} onChange={handleChange} />
+          <div>{errors.question && <span>{errors.question}</span>}</div>
+
+          <input className='inputUserName' type="text" placeholder='Text Answer' name='options[0]' value={values.options[0]} onChange={handleChange} />
+          <div>{errors.options && <span>{errors.options[0]}</span>}</div>
+
+          <input className='inputUserName' type="text" placeholder='Text Answer' name='options[1]' value={values.options[1]} onChange={handleChange} />
+          <div>{errors.options && <span>{errors.options[1]}</span>}</div>
+
+          <input className='inputUserName' type="text" placeholder='Text Answer' name='options[2]' value={values.options[2]} onChange={handleChange} />
+          <div>{errors.options && <span>{errors.options[2]}</span>}</div>
+
+          <input className='inputUserName' type="text" placeholder='Text Answer' name='options[3]' value={values.options[3]} onChange={handleChange} />
+          <div>{errors.options && <span>{errors.options[3]}</span>}</div>
          
           <button className='btn' type='submit'>Add Another Question</button>
         </div> 
@@ -39,5 +55,7 @@ const FormQuestions = () => {
     </div>
   );
 };
-    
+
 export default FormQuestions;
+
+
