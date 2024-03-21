5d1d4f0a0e6c415543b474ef89099bf2dd2b7ea6
cristiansot <soluciongrafix@gmail.com>
2024-03-21 15:38:15 -0300

    Developing Create component

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import FormQuestions from './FormQuestions';
-import '../assets/css/create.css'
+import '../assets/css/create.css';
+
+const schema = Yup.object({
+  name: Yup.string().required('Required'),
+});
 
 const Create = () => {
-/*I need to create ID*/
-  
+
+  const send = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
+  };
+
+  const { handleSubmit, handleChange, errors, values, setFieldValue } = useFormik({
+    initialValues: {
+      name: '',
+    },
+    validationSchema: schema,
+    onSubmit: send,
+  });
+
   return (
-    <div >
-        <div className='contentInputsLogin'>
+    <div className='contentInputsLogin'>
+      <form onSubmit={handleSubmit}>
         <h1 className='createQuizTitle'>Create Quiz</h1>
-          <input className='name' type="text" placeholder='Name of the Quiz' />
-    
-        </div> 
+        <input className='name' type="text" placeholder='Name of the Quiz' />
+
         <FormQuestions />
 
         <div className='btn'> 
           <button className='createBtn'>Create Quiz</button>
         </div>
-    </div>
-  );
-};
-    
+      </form>
+    </div> 
+  )};
+
 export default Create;
